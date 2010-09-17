9009071366fd3aec0e8002ac1c1fdf53da61d80f
mikkohei13 <mikkohei13@1e6c4c17-741f-60ae-fb77-7e84995b26cf>
2010-09-17 12:04:11 +0000

    First import

---
+++ include/include_umlauts.php
@@ -0,0 +1,284 @@
+<?php
+
+	// Lähde: http://www.ramsch.org/martin/uni/fmi-hp/iso8859-1.html
+
+	$uml[0]['uml'] = "&quot;";
+	$uml[0]['char'] = "\"";
+
+	$uml[1]['uml'] = "&amp;";
+	$uml[1]['char'] = "&";
+
+	$uml[2]['uml'] = "&lt;";
+	$uml[2]['char'] = "<";
+
+	$uml[3]['uml'] = "&gt;";
+	$uml[3]['char'] = ">";
+
+	$uml[4]['uml'] = "&iexcl;";
+	$uml[4]['char'] = "¡";
+
+	$uml[5]['uml'] = "&cent;";
+	$uml[5]['char'] = "¢";
+
+	$uml[6]['uml'] = "&pound;";
+	$uml[6]['char'] = "£";
+
+	$uml[7]['uml'] = "&curren;";
+	$uml[7]['char'] = "¤";
+
+	$uml[8]['uml'] = "&yen;";
+	$uml[8]['char'] = "¥";
+
+	$uml[9]['uml'] = "&brvbar;";
+	$uml[9]['char'] = "¦";
+
+	$uml[10]['uml'] = "&sect;";
+	$uml[10]['char'] = "§";
+
+	$uml[11]['uml'] = "&uml;";
+	$uml[11]['char'] = "¨";
+
+	$uml[12]['uml'] = "&copy;";
+	$uml[12]['char'] = "©";
+
+	$uml[13]['uml'] = "&ordf;";
+	$uml[13]['char'] = "ª";
+
+	$uml[14]['uml'] = "&laquo;";
+	$uml[14]['char'] = "«";
+
+	$uml[15]['uml'] = "&not;";
+	$uml[15]['char'] = "¬";
+
+	$uml[16]['uml'] = "&reg;";
+	$uml[16]['char'] = "®";
+
+	$uml[17]['uml'] = "&macr;";
+	$uml[17]['char'] = "¯";
+
+	$uml[18]['uml'] = "&deg;";
+	$uml[18]['char'] = "°";
+
+	$uml[19]['uml'] = "&plusmn;";
+	$uml[19]['char'] = "±";
+
+	$uml[20]['uml'] = "&sup2;";
+	$uml[20]['char'] = "²";
+
+	$uml[21]['uml'] = "&sup3;";
+	$uml[21]['char'] = "³";
+
+	$uml[22]['uml'] = "&acute;";
+	$uml[22]['char'] = "´";
+
+	$uml[23]['uml'] = "&micro;";
+	$uml[23]['char'] = "µ";
+
+	$uml[24]['uml'] = "&para;";
+	$uml[24]['char'] = "¶";
+
+	$uml[25]['uml'] = "&middot;";
+	$uml[25]['char'] = "·";
+
+	$uml[26]['uml'] = "&cedil;";
+	$uml[26]['char'] = "¸";
+
+	$uml[27]['uml'] = "&sup1;";
+	$uml[27]['char'] = "¹";
+
+	$uml[28]['uml'] = "&ordm;";
+	$uml[28]['char'] = "º";
+
+	$uml[29]['uml'] = "&raquo;";
+	$uml[29]['char'] = "»";
+
+	$uml[30]['uml'] = "&frac14;";
+	$uml[30]['char'] = "¼";
+
+	$uml[31]['uml'] = "&frac12;";
+	$uml[31]['char'] = "½";
+
+	$uml[32]['uml'] = "&frac34;";
+	$uml[32]['char'] = "¾";
+
+	$uml[33]['uml'] = "&iquest;";
+	$uml[33]['char'] = "¿";
+
+	$uml[34]['uml'] = "&Agrave;";
+	$uml[34]['char'] = "À";
+
+	$uml[35]['uml'] = "&Aacute;";
+	$uml[35]['char'] = "Á";
+
+	$uml[36]['uml'] = "&Acirc;";
+	$uml[36]['char'] = "Â";
+
+	$uml[37]['uml'] = "&Atilde;";
+	$uml[37]['char'] = "Ã";
+
+	$uml[38]['uml'] = "&Aring;";
+	$uml[38]['char'] = "Å";
+
+	$uml[39]['uml'] = "&AElig;";
+	$uml[39]['char'] = "Æ";
+
+	$uml[40]['uml'] = "&Ccedil;";
+	$uml[40]['char'] = "Ç";
+
+	$uml[41]['uml'] = "&Egrave;";
+	$uml[41]['char'] = "È";
+
+	$uml[42]['uml'] = "&Eacute;";
+	$uml[42]['char'] = "É";
+
+	$uml[43]['uml'] = "&Ecirc;";
+	$uml[43]['char'] = "Ê";
+
+	$uml[44]['uml'] = "&Igrave;";
+	$uml[44]['char'] = "Ì";
+
+	$uml[45]['uml'] = "&Iacute;";
+	$uml[45]['char'] = "Í";
+
+	$uml[46]['uml'] = "&Icirc;";
+	$uml[46]['char'] = "Î";
+
+	$uml[47]['uml'] = "&ETH;";
+	$uml[47]['char'] = "Ð";
+
+	$uml[48]['uml'] = "&Ntilde;";
+	$uml[48]['char'] = "Ñ";
+
+	$uml[49]['uml'] = "&Ograve;";
+	$uml[49]['char'] = "Ò";
+
+	$uml[50]['uml'] = "&Oacute;";
+	$uml[50]['char'] = "Ó";
+
+	$uml[51]['uml'] = "&Ocirc;";
+	$uml[51]['char'] = "Ô";
+
+	$uml[52]['uml'] = "&Otilde;";
+	$uml[52]['char'] = "Õ";
+
+	$uml[53]['uml'] = "&times;";
+	$uml[53]['char'] = "×";
+
+	$uml[54]['uml'] = "&Oslash;";
+	$uml[54]['char'] = "Ø";
+
+	$uml[55]['uml'] = "&Ugrave;";
+	$uml[55]['char'] = "Ù";
+
+	$uml[56]['uml'] = "&Uacute;";
+	$uml[56]['char'] = "Ú";
+
+	$uml[57]['uml'] = "&Ucirc;";
+	$uml[57]['char'] = "Û";
+
+	$uml[58]['uml'] = "&Yacute;";
+	$uml[58]['char'] = "Ý";
+
+	$uml[59]['uml'] = "&THORN;";
+	$uml[59]['char'] = "Þ";
+
+	$uml[60]['uml'] = "&agrave;";
+	$uml[60]['char'] = "à";
+
+	$uml[61]['uml'] = "&aacute;";
+	$uml[61]['char'] = "á";
+
+	$uml[62]['uml'] = "&acirc;";
+	$uml[62]['char'] = "â";
+
+	$uml[63]['uml'] = "&atilde;";
+	$uml[63]['char'] = "ã";
+
+	$uml[64]['uml'] = "&auml;";
+	$uml[64]['char'] = "ä";
+
+	$uml[65]['uml'] = "&aring;";
+	$uml[65]['char'] = "å";
+
+	$uml[66]['uml'] = "&aelig;";
+	$uml[66]['char'] = "æ";
+
+	$uml[67]['uml'] = "&ccedil;";
+	$uml[67]['char'] = "ç";
+
+	$uml[68]['uml'] = "&egrave;";
+	$uml[68]['char'] = "è";
+
+	$uml[69]['uml'] = "&eacute;";
+	$uml[69]['char'] = "é";
+
+	$uml[70]['uml'] = "&ecirc;";
+	$uml[70]['char'] = "ê";
+
+	$uml[71]['uml'] = "&euml;";
+	$uml[71]['char'] = "ë";
+
+	$uml[72]['uml'] = "&igrave;";
+	$uml[72]['char'] = "ì";
+
+	$uml[73]['uml'] = "&iacute;";
+	$uml[73]['char'] = "í";
+
+	$uml[74]['uml'] = "&icirc;";
+	$uml[74]['char'] = "î";
+
+	$uml[75]['uml'] = "&iuml;";
+	$uml[75]['char'] = "ï";
+
+	$uml[76]['uml'] = "&eth;";
+	$uml[76]['char'] = "Ð";
+
+	$uml[77]['uml'] = "&ntilde;";
+	$uml[77]['char'] = "ñ";
+
+	$uml[78]['uml'] = "&ograve;";
+	$uml[78]['char'] = "ò";
+
+	$uml[79]['uml'] = "&oacute;";
+	$uml[79]['char'] = "ó";
+
+	$uml[80]['uml'] = "&ocirc;";
+	$uml[80]['char'] = "ô";
+
+	$uml[81]['uml'] = "&otilde;";
+	$uml[81]['char'] = "õ";
+
+	$uml[82]['uml'] = "&ouml;";
+	$uml[82]['char'] = "ö";
+
+	$uml[83]['uml'] = "&divide;";
+	$uml[83]['char'] = "÷";
+
+	$uml[84]['uml'] = "&oslash;";
+	$uml[84]['char'] = "ø";
+
+	$uml[85]['uml'] = "&ugrave;";
+	$uml[85]['char'] = "ù";
+
+	$uml[86]['uml'] = "&uacute;";
+	$uml[86]['char'] = "ú";
+
+	$uml[87]['uml'] = "&ucirc;";
+	$uml[87]['char'] = "û";
+
+	$uml[88]['uml'] = "&uuml;";
+	$uml[88]['char'] = "ü";
+
+	$uml[89]['uml'] = "&yacute;";
+	$uml[89]['char'] = "ý";
+
+	$uml[90]['uml'] = "&thorn;";
+	$uml[90]['char'] = "þ";
+
+	$uml[91]['uml'] = "&yuml;";
+	$uml[91]['char'] = "ÿ";
+	
+//	print_r ($uml);
+
+
+?>
